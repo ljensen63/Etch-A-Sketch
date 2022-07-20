77a191e905092653853587217e24f0cdccc66d44
Liam Jensen <jensen97@purdue.edu>
2022-07-19 23:39:39 -0400

    remove borders on grid and attempt to grow grid to size of page

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ console.log(gridSpace);
 for (let i = 0; i < 256; i++){
     let gridSquare = document.createElement('div');
     gridSquare.classList.add('cell');
+    gridSquare.style.minHeight = '10px';
+    gridSquare.style.minWidth = '10px';
+    console.log(gridSpace.style.maxWidth);
     gridSpace.appendChild(gridSquare);
 
 }
