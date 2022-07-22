807990980e07154628c2a3b395c7eec8debf4f7a
Liam Jensen <jensen97@purdue.edu>
2022-07-22 14:46:13 -0400

    add eventlistener for button to create new grid

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ for (let i = 0; i < 256; i++){
     gridSpace.appendChild(gridSquare);
 
 }
+//given the container & new size for the grid we remove the container and then replace it with new grid container
+function replace(gridSpace){
+    let newSize = prompt('Please enter new size of grid');
+    console.log(newSize);
+    document.body.removeChild(gridSpace);
+    gridSpace = document.createElement('div');
+    gridSpace.classList.add('grid-space');
+    gridSpace.style.border = '2px solid black'
+    document.body.appendChild(gridSpace);
+    for(let k = 0; k < newSize * newSize;k++){
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('cell');
+        gridSquare.style.minHeight = '10px';
+        gridSquare.style.minWidth = '10px';
+        gridSpace.appendChild(gridSquare);
+    } 
+}
+
+let newGrid = document.querySelector('.new-Grid');
+newGrid.addEventListener('click', replace(gridSpace));
