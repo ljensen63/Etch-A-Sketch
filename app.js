c631759a104f28c87c2a4f9883695ce8ca89f2e4
Liam Jensen <jensen97@purdue.edu>
2022-07-28 15:56:20 -0400

    add height to grid container, clear button, remove old comments

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const gridContainer = document.querySelector('.grid-container');
 const colorButtonsContainer = document.querySelector('.color-Buttons');
 const colorButtons = colorButtonsContainer.querySelectorAll('button');
+const clearButton = document.querySelector('.clear');
+
 let currentColor;
 colorButtons.forEach(colorButtons => colorButtons.addEventListener('click', () => {
     currentColor = colorButtons.classList[0];
 }));
-//let currentColor = 
 
-//given the container & new size for the grid we remove the container and then replace it with new grid container
+clearButton.addEventListener('click', () => {
+    let cells = gridContainer.querySelectorAll('div');
+    cells.forEach(cells => clear(cells));
+});
+
+
+//given new size for the grid we remove each cell and then replace it with new grid cells with the new size given
 function newGrid(gridSize){
     let cells = gridContainer.querySelectorAll('div');
     cells.forEach(cells => cells.remove()); 
@@ -21,39 +28,25 @@ function newGrid(gridSize){
         gridContainer.insertAdjacentElement('beforeend', gridCell);
     }
     cells = gridContainer.querySelectorAll('div');
-    cells.forEach(cells => cells.addEventListener('mouseover', () => { addColor()}));
-    /*
-    //console.log(newSize);
-    //console.log(document.body.childNodes);
-    let gridSpace2 = document.querySelector('.grid-space')
-    gridSpace2.removeChild(gridSpace);
-    gridSpace = document.createElement('div');
-    gridSpace.classList.add('grid');
-    gridSpace.style.border = '2px solid black'
-    document.body.appendChild(gridSpace);
-    for(let k = 0; k < newSize;k++){
-        for (let a = 0; a < newSize; a++){
-            let gridSquare = document.createElement('div');
-            gridSquare.classList.add('cell');
-            gridSquare.style.minHeight = '10px';
-            gridSquare.style.minWidth = '10px';
-            gridSpace.appendChild(gridSquare);
-        }
-        let breaker = document.createElement('br');
-        gridSpace.appendChild(breaker);
-    } */
-
+    cells.forEach(cells => cells.addEventListener('mouseover', (e) => { addColor(e.target)}));
+}
+//clears classlist of cell
+function clear(cell){
+    let classList = cell.classList;
+    while (classList.length > 0) {
+        classList.remove(classList.item(0));
+    }
 }
 
-function addColor(){
-    let cells = gridContainer.querySelectorAll('div');
-    cells.forEach(cells => cells.classList.add(currentColor));
+//adds currentColor to classlist of cell
+function addColor(e){
+    clear(e);
+    e.classList.add(currentColor);
 }
 
 let newGridButton = document.querySelector('.new-Grid');
 newGridButton.addEventListener('click', () => {
-    let gridsize = prompt('New Grid Size?')
-    //need to figure out why DOM doesn't have the grid as a child of the grid-space when it is getting removed
+    let gridsize = prompt('New Grid Size?');
     newGrid(gridsize);
 });
 
